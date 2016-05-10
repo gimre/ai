b880bd2f227bd96702714b744b4c6cebb3f124e8
Gabriel Imre <gimre@endava.net>
2016-05-10 14:12:19 +0300

    added 1. and 2. for C1

--- lab01/c1.py
+++ lab01/c1.py
@@ -1,14 +1,48 @@
 import unittest
 
 class C1( ):
-    def __init__( self ):
-        self.x = 2
-    def add( self, value ):
-        self.x += value
+    def __init__( self, text ):
+        self.text = text
 
-class C1Test( unittest.TestCase ):
-    def testC1( self ):
-        self.assertTrue( True )
+    def longestWord( self ):
+        words     = list( reversed( self.text.split( ) ) )
+        lengths   = [ len( word ) for word in words ]
+        maxLength = max( lengths )
+        return words[ lengths.index( maxLength ) ]
+
+    def frequency( self ):
+        words   = self.text.split( )
+        freqMap = {}
+        for word in words:
+            freqMap[ word ] = freqMap[ word ] + 1 if word in freqMap else 1
+        return freqMap
+
+
+class TestLongestWord( unittest.TestCase ):
+    def testSingle( self ):
+        self.assertEqual( C1( 'a very clear longest word' ).longestWord( ), 'longest' )
+    def testMultiple( self ):
+        self.assertEqual( C1( 'this is a nice text' ).longestWord( ), 'text' )
+
+class TestFrequency( unittest.TestCase ):
+    def testOne( self ):
+        self.assertEqual( C1( 'this is a nice text' ).frequency( ), {
+            'a': 1,
+            'is': 1,
+            'nice': 1,
+            'text': 1,
+            'this': 1
+        } )
+    def testTwo( self ):
+        self.assertEqual( C1( 'this like is the best like frequency like test' ).frequency( ), {
+            'best': 1,
+            'is': 1,
+            'frequency': 1,
+            'like': 3,
+            'test': 1,
+            'the': 1,
+            'this': 1
+        } )
 
 if __name__ == '__main__':
     unittest.main( )
